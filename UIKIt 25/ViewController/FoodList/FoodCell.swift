4ca5e8fa81dd6a49e6583e0785d1e4db665907c6
rezaulislam <mrezaulislam.t@gmail.com>
2025-11-13 13:07:38 +0600

    table view with UITableViewDiffableDataSource

---
+++ UIKIt 25/ViewController/FoodList/FoodCell.swift
@@ -0,0 +1,50 @@
+//
+//  FoodCell.swift
+//  UIKIt 25
+//
+//  Created by Rezaul Islam on 13/11/25.
+//
+
+import UIKit
+
+class FoodCell: UITableViewCell {
+    static let reuseIdentifier = "FoodCell"
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        label.text = "Title"
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        contentView.addSubview(label)
+    }
+    
+    private func setupConstraint(){
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+        ])
+    }
+    
+    func config(with food: FoodItem){
+        label.text = food.name
+    }
+    
+    
+}
