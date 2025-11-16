4e7b7a519316404e6ee2529aae53c1207ffc6268
rezaulislam <mrezaulislam.t@gmail.com>
2025-11-16 22:05:39 +0600

    Ambulance service added

--- UIKIt 25/ViewController/MultiListApp/Services/AmbulanceService.swift
+++ UIKIt 25/ViewController/MultiListApp/Services/AmbulanceService.swift
@@ -6,7 +6,34 @@
 //
 
 import Foundation
+import Combine
 
-public protocol AmbulanceService {
-    func getAmbulance() -> [Ambulance]
+protocol AmbulanceServiceProtocol {
+    func getAmbulance() async throws -> [Transport]
+}
+
+class AmbulanceService : AmbulanceServiceProtocol {
+    private var cancellable: Set<AnyCancellable> = []
+    func getAmbulance() async throws -> [Transport] {
+        return try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<[Transport], Error>) in
+            Network.shared.easyNet.fetchData(endPoint: Endpoints.getAmbulance.path, responseType: TransportResponse.self)
+                .sink { completion in
+                    switch completion {
+                    case .failure(let error):
+                        continuation.resume(throwing: error)
+                    case .finished:
+                        break
+                    }
+                } receiveValue: { response in
+                    continuation.resume(returning: response.data)
+                }
+                .store(in: &cancellable)
+        }
+    }
+}
+
+class FakeAmbulanceService : AmbulanceServiceProtocol {
+    func getAmbulance() async throws -> [Transport] {
+        return Transport.mockList
+    }
 }
