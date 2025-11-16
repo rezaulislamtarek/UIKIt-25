db6b0c8d45c5af739ccd05cf368c4aa8ab6df0d0
rezaulislam <mrezaulislam.t@gmail.com>
2025-11-16 22:01:11 +0600

    add endpoint for smb

--- UIKIt 25/Networking/Network.swift
+++ UIKIt 25/Networking/Network.swift
@@ -10,19 +10,6 @@ import EasyNet
 
 class Network {
     static let shared = Network()
-    
-    private let apiKey = "EbRZip3wkVozNuwxPKSHGA==burTpVYa05VMuDTB"
-    
-    // শুধু base URL দেবে — query এখানে দিবে না!
-    let easyNet = EasyNet(baseUrl: "https://api.api-ninjas.com/v1", enableDebugLogging: true)
-    
-    private init() {
-        configure()
-    }
-    
-    private func configure() {
-        easyNet.setHeaders(headers: [
-            "X-Api-Key": apiKey
-        ])
-    }
+    let easyNet = EasyNet(baseUrl: "https://api.smartbancharampur.com/api", enableDebugLogging: true)
+    private init() { }
 }
